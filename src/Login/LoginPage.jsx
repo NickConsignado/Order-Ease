b579301021bf460295ea8a60b95c84683a4386fe
Nick Consignado <nickconsignado@gmail.com>
2023-08-01 07:31:23 +0800

    setting up kitchen home page

--- src/Login/LoginPage.jsx
+++ src/Login/LoginPage.jsx
@@ -1,13 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-function LoginPage (){
-    return(
-        <>
-        <Link to="/home-cashier"><p>Cashier</p></Link>
-        <Link to="/home-kitchen"><p>Kitchen</p></Link>
-        <Link to="/home-food-server"><p>FoodServer</p></Link>
-        </>
-    )
+function LoginPage() {
+  return (
+    <>
+      <div>
+        <Link to="/home-cashier">
+          <p>Cashier</p>
+        </Link>
+        <Link to="/home-kitchen">
+          <p>Kitchen</p>
+        </Link>
+        <Link to="/home-food-server">
+          <p>FoodServer</p>
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/kitchen/HomeKitchen.jsx
+++ src/kitchen/HomeKitchen.jsx
@@ -1,7 +1,183 @@
-function HomeKitchen (){
-    return(
-        <>Kitchen Page</>
-    )
+import { NavLink } from "react-router-dom";
+
+function HomeKitchen() {
+  return (
+    <>
+      <nav className="navbar bg-light fixed-top">
+        <div className="container-fluid">
+          <div>
+            <NavLink style={{ textDecoration: "none" }}>
+              <span className="navbar-brand">Order/s</span>
+            </NavLink>
+            <NavLink style={{ textDecoration: "none" }}>
+              <span className="navbar-brand">Inventory</span>
+            </NavLink>
+          </div>
+          <h1>Logo</h1>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="offcanvas offcanvas-end"
+            tabindex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                Kitchen
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div>
+        Kitchen Page
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        Hello world
+      </div>
+    </>
+  );
 }
 
-export default HomeKitchen
+export default HomeKitchen;

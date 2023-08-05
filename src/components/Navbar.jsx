8c0bc02386145e19648b317312f5db516fdd9034
Ederick S. Banez <esb.tailor@gmail.com>
2023-08-05 14:30:25 +0800

    addded components offcanvas hamburger, LefAside, Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return{
+        <>
+            <nav className="navbar fixed-top bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Fixed top</a>
+                </div>
+            </nav>
+        </>
+    }
+}
+
+export default Navbar;
